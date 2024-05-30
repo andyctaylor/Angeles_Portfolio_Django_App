0abcd24b343fe80a448782300fedd594981a6330
Andy Taylor <angelusctaylor@gmail.com>
2024-05-30 12:04:23 +0400

    Update project build.sh again

--- build.sh
+++ build.sh
@@ -3,14 +3,6 @@
 # Exit immediately if a command exits with a non-zero status.
 set -o errexit
 
-# Update package lists
-echo "Updating package lists..."
-sudo apt-get update
-
-# Activate the virtual environment
-echo "Activating virtual environment..."
-source venv/bin/activate
-
 # Install Python dependencies
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
